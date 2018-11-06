04d8959431aaf84f6258b37f4df6adb9073a67b7
Will Xu <1053334228@qq.com>
2018-11-06 16:42:18 +0800

    初始化

---
+++ code/review/src/main/java/com/neverxp/hashmap/My_HashMap.java
@@ -0,0 +1,14 @@
+package com.neverxp.hashmap;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by xup-e on 2018/11/6.
+ */
+public class My_HashMap {
+
+    public static void main(String[] args) {
+        Map map = new HashMap();
+    }
+}
