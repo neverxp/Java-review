c9a04ff1c94febe262d60070dd53d0a54d98996a
Will Xu <1053334228@qq.com>
2018-11-06 17:08:52 +0800

    hello world

---
+++ code/review/src/main/java/com/neverxp/hashmap/TestHashMap.java
@@ -0,0 +1,10 @@
+package com.neverxp.hashmap;
+
+/**
+ * Created by xup-e on 2018/11/6.
+ */
+public class TestHashMap {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
